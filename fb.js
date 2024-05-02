807360044eef6f0028e45f129f60f125b9767b35
Chang94033143 <144894245+Chang94033143@users.noreply.github.com>
2024-05-02 11:40:31 +0800

    fb.js

--- fb.js
+++ fb.js
@@ -1,23 +1,16 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+function register() {
+            var username = document.getElementById('username').value;
+            var password = document.getElementById('password').value;
 
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-    fetch('a.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: 'username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password),
-    })
-    .then(response => response.text())
-    .then(data => console.log(data))
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-});
-
-
-
+            
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'register.php', true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    alert(xhr.responseText);
+                }
+            };
+            xhr.send('username=' + username + '&password=' + password);
+        }
 
